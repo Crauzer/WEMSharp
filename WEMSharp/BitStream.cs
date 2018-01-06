b0394f9e6aa0b69b1f906aa5e05c2cd967039697
Crauzer <filip.quitko@gmail.com>
2018-01-06 19:14:23 +0100

    Initial commit

---
+++ WEMSharp/BitStream.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+
+namespace WEMSharp
+{
+    internal class BitStream
+    {
+        private Stream _stream;
+        private byte _bitBuffer;
+        private int _bitsLeft;
+        internal ulong TotalBitsRead { get; private set; }
+
+        public BitStream(Stream stream)
+        {
+            this._stream = stream;
+        }
+
+        internal byte GetBit()
+        {
+            if (this._bitsLeft == 0)
+            {
+                this._bitBuffer = (byte)this._stream.ReadByte();
+                this._bitsLeft = 8;
+            }
+
+            this.TotalBitsRead++;
+            this._bitsLeft--;
+
+            return (this._bitBuffer & (0x80 >> this._bitsLeft)) != 0 ? (byte)1 : (byte)0;
+        }
+
+        internal uint Read(int bitCount)
+        {
+            uint result = 0;
+
+            for(int i = 0; i < bitCount; i++)
+            {
+                if(GetBit() == 1)
+                {
+                    result |= (1U << i);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ WEMSharpTests/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WEMSharp;
+
+namespace WEMSharpTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WEMFile wem = new WEMFile("LoL_MUSIC_TreelineINTRO.wem", WEMForcePacketFormat.ForceNoModPackets);
+            wem.GenerateOGG("LoL_MUSIC_TreelineINTRO.ogg", "packed_codebooks_aoTuV_603.bin", false, false);
+        }
+    }
+}
